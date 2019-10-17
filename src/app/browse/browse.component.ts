6d23abc2a3f39074bf1e1d53e514b25d256da285
Triet Le <lehoang03triet@gmail.com>
2019-10-17 18:21:28 +0700

    Add data list

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import * as faker from "faker";
+
+interface IPerson {
+    name: string;
+    email: string;
+    address: string;
+    description: string;
+}
 
 @Component({
     selector: "Browse",
     templateUrl: "./browse.component.html"
 })
 export class BrowseComponent implements OnInit {
+    persons: Array<IPerson> = [];
 
     constructor() {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
-        // Init your component properties here.
+        this.persons = Array.apply(null, { length: 10 }).map(
+            (): IPerson => ({
+                name: faker.name.findName(),
+                email: faker.internet.email(),
+                address: faker.address.streetAddress(true),
+                description: faker.lorem.paragraphs(1)
+            })
+        );
+
+        console.log("this.persons.length = ", this.persons.length);
+        console.log("this.persons[0] = ", this.persons[0]);
     }
 
     onDrawerButtonTap(): void {
--- src/app/browse/browse.module.ts
+++ src/app/browse/browse.module.ts
@@ -1,19 +1,13 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NativeScriptUIListViewModule } from "nativescript-ui-listview/angular";
 
 import { BrowseRoutingModule } from "./browse-routing.module";
 import { BrowseComponent } from "./browse.component";
 
 @NgModule({
-    imports: [
-        NativeScriptCommonModule,
-        BrowseRoutingModule
-    ],
-    declarations: [
-        BrowseComponent
-    ],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+    imports: [NativeScriptCommonModule, BrowseRoutingModule, NativeScriptUIListViewModule],
+    declarations: [BrowseComponent],
+    schemas: [NO_ERRORS_SCHEMA]
 })
-export class BrowseModule { }
+export class BrowseModule {}
